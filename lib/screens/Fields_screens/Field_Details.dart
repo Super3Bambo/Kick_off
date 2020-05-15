a6eef15b4338f06b45e25a51877de27ff93d4c02
khaleddmohamedd <khaledhany007@gmail.com>
2020-05-15 22:05:09 +0200

    local 2

--- lib/screens/Fields_screens/Field_Details.dart
+++ lib/screens/Fields_screens/Field_Details.dart
@@ -83,6 +83,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_app/Services/Fields.dart';
 import 'package:flutter_app/Services/User.dart';
 import 'package:flutter_app/Shared/Loading.dart';
+import 'package:flutter_app/screens/Matches/Match_Details_Complete.dart';
 import 'package:random_string_one/random_string.dart';
 import '../../models/field.dart';
 import '../../Services/Match.dart';
@@ -90,12 +91,17 @@ import '../../models/User.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 
 class FieldDetails extends StatefulWidget {
   final Field fieldid;
 
   FieldDetails({this.fieldid});
+
+
+
+
 
   @override
   _FieldDetailsState createState() => _FieldDetailsState();
@@ -111,14 +117,41 @@ bool loading = false;
 final Firestore _db = Firestore.instance;
   final FirebaseMessaging _fcm = FirebaseMessaging();
 
+  
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin= new FlutterLocalNotificationsPlugin();
+var initilizationSettingsAndroid;
+var initilizationSettings;
+var initilizationSettingsIOS;
+
+void _showNotification()async{
+await _demoNotification();
+}
+
+Future<void> _demoNotification()async{
+ var androidPlateform= AndroidNotificationDetails('channel ID','channel name','chaneel Description',importance: Importance.Max,priority: Priority.High,ticker: 'text ticker');
+ var iosPlateform=IOSNotificationDetails();
+  var plateformChannel=NotificationDetails(androidPlateform,iosPlateform);
+  await flutterLocalNotificationsPlugin.show(0, 'A new Match', 'Come and join us!', plateformChannel,payload: 'test payload');
+}
+
 
   @override
   void initState() {
     super.initState();
-   
+    initilizationSettingsAndroid = new AndroidInitializationSettings('app_icon');
+  initilizationSettings=new InitializationSettings(initilizationSettingsAndroid, initilizationSettingsIOS);
+  initilizationSettingsIOS = new IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    initilizationSettings = new InitializationSettings(
+        initilizationSettingsAndroid, initilizationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initilizationSettings,
+        onSelectNotification: onSelectNotification);
 
     _fcm.configure(
       onMessage: (Map<String, dynamic> message) async {
+          _showNotification();
+
+ 
         print("onMessage: $message");
         // final snackbar = SnackBar(
         //   content: Text(message['notification']['title']),
@@ -129,7 +162,7 @@ final Firestore _db = Firestore.instance;
         // );
 
         // Scaffold.of(context).showSnackBar(snackbar);
-        showDialog(
+       /* showDialog(
           context: context,
           builder: (context) => AlertDialog(
                 content: ListTile(
@@ -144,7 +177,7 @@ final Firestore _db = Firestore.instance;
                   ),
                 ],
               ),
-        );
+        );*/
       },
       onLaunch: (Map<String, dynamic> message) async {
         print("onLaunch: $message");
@@ -156,6 +189,36 @@ final Firestore _db = Firestore.instance;
       },
     );
   }
+
+Future onDidReceiveLocalNotification(
+      int id, String title, String body, String payload) async {
+    await showDialog(
+        context: context,
+        builder: (BuildContext context) => CupertinoAlertDialog(
+              title: Text(title),
+              content: Text(body),
+              actions: <Widget>[
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: Text('Ok'),
+                  onPressed: () async {
+                    Navigator.of(context, rootNavigator: true).pop();
+                    await Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Match_Details()));
+                  },
+                )
+              ],
+            ));
+
+
+    }
+Future onSelectNotification(String payload)async{
+if(payload!=null){
+  debugPrint('Notification Payload : $payload');
+}
+await Navigator.push(context, new MaterialPageRoute(builder: (context) =>new Match_Details()));
+}
+
 
   @override
   void dispose() {
